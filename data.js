fce969477f687c38fd2213a2d7de39abc693d9a2
admin <murtuza.xworkz@gmail.com>
2023-12-30 13:17:05 +0530

    check file name

---
+++ data.js
@@ -0,0 +1,63 @@
+
+let batchList = [
+
+    {
+        "batch": "june 5",
+        "trainer": "dev sir",
+        "topic": "collection"
+    }, {
+        "batch": "nov",
+        "trainer": "om sir",
+        "topic": "exception"
+    },
+    {
+        "batch": "nov",
+        "trainer": "om sir",
+        "topic": "exception"
+    }
+]
+
+let weeklyMock = [
+
+    {
+        "batch": "june 5",
+        "mock_spoc": "Hareesha",
+        "topic": "collection"
+    }, {
+        "batch": "nov",
+        "mock_spoc": "old students",
+        "topic": "exception"
+    },
+    {
+        "batch": "nov",
+        "mock_spoc": "om sir",
+        "topic": "exception"
+    }
+]
+
+
+
+let upcomingBatch = [
+
+    {
+        "batch": "jan 5",
+        "trainer": "dev sir",
+        "location": "Rajajinagar"
+    }, {
+        "batch": "feb 10",
+        "trainer": "om sir",
+        "location": "Rajajinagar"
+    },
+    {
+        "batch": "feb 12",
+        "trainer": "hareesha",
+        "location": "BTM"
+    }
+]
+
+
+
+
+
+
+
